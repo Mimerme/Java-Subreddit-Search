9364c640b6f2adba198af098d78f07f151c28a59
Mimerme <androssdragonq.9@gmail.com>
2015-05-30 09:56:43 -0400

    First Commit

---
+++ src/main/java/io/github/mimerme/Java_Subreddit_Search/App.java
@@ -0,0 +1,9 @@
+package io.github.mimerme.Java_Subreddit_Search;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
